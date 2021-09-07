6097efbf4a1f97b409cd3293a696a638afabb281
Jimin Kim <0305jimin@gmail.com>
2021-09-08 00:05:14 +0900

    week 2 #5,6,7

--- 2주차.c/문제5.c
+++ 2주차.c/문제5.c
@@ -4,9 +4,15 @@
 //
 //  Created by 김지민 on 2021/09/07.
 //
-
-#include <stdio.h>
-
-int main() {
-    
-}
+//
+//#include <stdio.h>
+//
+//int main() {
+//    int point1 = 95;
+//    int point2 = 84;
+//    int total = point1+point2;
+//    printf("%d\n",total);
+//    
+//    return 0;
+//    
+//}
---
+++ 문제6.c
@@ -0,0 +1,18 @@
+//
+//  문제6.c
+//  2주차.c
+//
+//  Created by 김지민 on 2021/09/07.
+//
+
+//#include <stdio.h>
+//
+//#undef _CRT_SECURE_NO_WARNINGS
+//int main() {
+//    float num1=0;
+//    float num2 = 0;
+//    scanf("%f %f",&num1, &num2);
+//    printf("%f %f\n",num1+num2,(num1+num2)/2);
+//    
+//    return 0;
+//}
