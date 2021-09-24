273735cb5accff743cc35df791e16964ca7f5136
Jimin Kim <0305jimin@gmail.com>
2021-09-24 21:32:25 +0900

    Feat: adding exception handling function #6

--- 3주차/3주차/7.c
+++ 3주차/3주차/7.c
@@ -5,14 +5,14 @@
 //  Created by 김지민 on 2021/09/15.
 //
 
-#include <stdio.h>
-
-int main(void){
-    int sum=0;
-       int i=1;
-    for (i=1; sum<=100000; i++) {
-        sum+=i;
-    }
-       printf("sum:%d n:%d\n",sum-i,i-1);
-       return 0;
-}
+//#include <stdio.h>
+//
+//int main(void){
+//    int sum=0;
+//       int i=1;
+//    for (i=1; sum<=100000; i++) {
+//        sum+=i;
+//    }
+//       printf("sum:%d n:%d\n",sum-i,i-1);
+//       return 0;
+//}
