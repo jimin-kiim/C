3c39f03fa85b3f1bde20205f1f5ccac3ca23b75d
Jimin Kim <0305jimin@gmail.com>
2021-09-29 14:03:41 +0900

    Refactor: code review

--- 5주차/5주차/4.c
+++ 5주차/5주차/4.c
@@ -8,6 +8,18 @@
 //#include <stdio.h>
 //
 //int main(void) {
+//    int scores[][4]={97,90,88,95,76, 89, 75, 83,60, 70, 88, 82, 83, 89, 92, 85, 75, 73, 72, 78};
+//    int sum=0;
+//    printf("Quiz Midterm Report Final   Sum  Average\n");
+//    printf("========================================\n");
+//    for (int i=0; i<5; i++){
+//        for(int j=0; j<4; j++){
+//            printf(" %d    ",scores[i][j]);
+//            sum+=scores[i][j];
+//        }
+//        printf("%d    %d\n",sum,sum/4);
+//        sum=0;
+//    }
 //    
 //    return 0;
 //}
