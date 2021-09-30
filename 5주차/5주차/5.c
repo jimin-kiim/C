2d6773fd95f82cd10acc03ec6a7029d326d35af3
Jimin Kim <0305jimin@gmail.com>
2021-09-30 22:53:53 +0900

    Refactor: week 5

--- 5주차/5주차/5.c
+++ 5주차/5주차/5.c
@@ -4,20 +4,22 @@
 //
 //  Created by 김지민 on 2021/09/27.
 //
-
-#include <stdio.h>
-
-int main(void){
-    int data1 = 10, data2 = 20;
-    int sum=0;
-    int *p;
-    p=&data1;
-    *p=100;
-    p=&data2;
-    *p=200;
-    sum+=*p;
-    p=&data1;
-    sum+=*p;
-    printf("%d\n",sum);
-    return 0;
-}
+//
+//#include <stdio.h>
+//
+//int main(void){
+//    int data1 = 10, data2 = 20;
+//    int sum=0;
+////    int *p=NULL;
+////    p=&data1;
+//    int *p = &data1;
+//    *p=100;
+//    sum+=*p;
+//    
+//    p=&data2;
+//    *p=200;
+//    sum+=*p;
+//    
+//    printf("%d\n",sum);
+//    return 0;
+//}
--- 5주차/5주차/7.c
+++ 5주차/5주차/7.c
@@ -5,15 +5,15 @@
 //  Created by 김지민 on 2021/09/27.
 //
 //
-//#include <stdio.h>
-//
-//int main(void){
-//    int data[] = {3, 21, 35, 57, 24, 82, 8};
-//    
-//    for(int i=0; i<sizeof(data)/4;i++){
-//        *(data+i)+=10;
-//        printf("%d\n",data[i]);
-//    }
-//    
-//    return 0;
-//}
+#include <stdio.h>
+
+int main(void){
+    int data[] = {3, 21, 35, 57, 24, 82, 8};
+    int size=sizeof(data)/sizeof(data[0]);
+    for(int i=0; i<size;i++){
+        *(data+i)+=10;
+        printf("%d\n",data[i]);
+    }
+    
+    return 0;
+}
