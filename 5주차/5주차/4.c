c3153fcb31387b2ff7a4cccd7a45cdbd693a9492
Jimin Kim <0305jimin@gmail.com>
2021-10-02 00:12:47 +0900

    Refactor: week 5 #4,6,7

--- 5주차/5주차/4.c
+++ 5주차/5주차/4.c
@@ -4,20 +4,23 @@
 //
 //  Created by 김지민 on 2021/09/27.
 //
-
+//
 //#include <stdio.h>
 //
 //int main(void) {
 //    int scores[][4]={97,90,88,95,76, 89, 75, 83,60, 70, 88, 82, 83, 89, 92, 85, 75, 73, 72, 78};
-//    int sum=0;
-//    printf("Quiz Midterm Report Final   Sum  Average\n");
-//    printf("========================================\n");
-//    for (int i=0; i<5; i++){
-//        for(int j=0; j<4; j++){
+//    char name[][10]={"Gildong","James","Harry","Jack","Changsoo"};
+//    int i,j,sum=0;
+//    
+//    printf("        Quiz Midterm Report Final   Sum  Average\n");
+//    printf("================================================\n");
+//    for (i=0; i<5; i++){
+//        printf("%8s",name[i]);
+//        for(j=0; j<4; j++){
 //            printf(" %d    ",scores[i][j]);
 //            sum+=scores[i][j];
 //        }
-//        printf("%d    %d\n",sum,sum/4);
+//        printf("%d    %.2f\n",sum,(float)sum/4);
 //        sum=0;
 //    }
 //    
--- 5주차/5주차/7.c
+++ 5주차/5주차/7.c
@@ -10,10 +10,14 @@
 //int main(void){
 //    int data[] = {3, 21, 35, 57, 24, 82, 8};
 //    int size=sizeof(data)/sizeof(data[0]);
-//    for(int i=0; i<size;i++){
-//        *(data+i)+=10;
-//        printf("%d\n",data[i]);
-//    }
+//    const int inc =10;
 //    
+//    printf("배열의 원소 값 출력:\n");
+//    
+//    for(int i=0; i<size;i++){
+//        *(data+i)+=inc;
+//        printf("%d ",data[i]);
+//    }
+//    printf("\n");
 //    return 0;
 //}
