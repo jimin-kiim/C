8b844fb7fece5f176d3187c43633c9a3b7d2494c
Jimin Kim <0305jimin@gmail.com>
2021-10-08 00:19:38 +0900

    week 6 #5

--- 6주차/6주차/5.c
+++ 6주차/6주차/5.c
@@ -5,4 +5,30 @@
 //  Created by 김지민 on 2021/10/07.
 //
 
-#include "5.h"
+#include <stdio.h>
+
+char score2grade(float);
+
+int main(void){
+    
+    float score;
+    
+    printf("점수를 입력해주세요: ");
+    scanf("%f", &score);
+    printf("학점: %c\n",score2grade(score));
+}
+
+char score2grade(float score) {
+    
+    if (score>=90)
+        return 'A';
+    if(score>=80)
+        return 'B';
+    if(score>=70)
+        return 'C';
+    if(score>=60)
+        return 'D';
+    return 'F';
+    
+    
+}
