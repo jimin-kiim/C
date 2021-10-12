c46f5d9a1cb66124b8bd8d9869bc1373fdb92ba5
Jimin Kim <0305jimin@gmail.com>
2021-10-12 23:59:50 +0900

    week 7 #7

---
+++ week 7/week 7/7.c
@@ -0,0 +1,19 @@
+//
+//  7.c
+//  week 7
+//
+//  Created by 김지민 on 2021/10/12.
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[] ){
+    int i = 0;
+    printf("실행 명령행 인자(command line arguments) >>\n");
+    printf("argc = %d\n", argc);
+    for (i = 0; i < argc; i++)
+           printf("argv[%d] = %s\n", i, argv[i]);
+    printf("%d+ %d=%d",argc, atoi(argv[1]),argc+ atoi(argv[1]));
+    return 0;
+}
