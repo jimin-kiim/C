9863deb8923f21113716b587a21fbeebcb8a4786
Jimin Kim <0305jimin@gmail.com>
2021-10-13 13:57:14 +0900

    week 7 #8

--- week 7/week 7/8.c
+++ week 7/week 7/8.c
@@ -5,4 +5,23 @@
 //  Created by 김지민 on 2021/10/13.
 //
 
-#include "8.h"
+#include <stdio.h>
+#include <string.h>
+int mystrlen(const char *p);
+int main() {
+    char str[100];
+    printf("Enter any string:");
+    scanf("%s", str);
+    printf("Using mystrlen, '%s' is : %d\n",str, mystrlen(str));
+    printf("Using strlen, '%s' is : %d\n",str, strlen(str));
+    return 0;
+}
+
+int mystrlen(const char *p) {
+    int count=0;
+    while(*p){
+        count++;
+        p++;
+    }
+    return count;
+}
