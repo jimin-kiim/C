5dbff714c0b1e797b23de776854df03208af1a62
Jimin Kim <0305jimin@gmail.com>
2021-10-14 23:42:04 +0900

    Refactor: week 7 #5
    moving printf out of the user defined function

--- week 7/week 7/5.c
+++ week 7/week 7/5.c
@@ -4,23 +4,27 @@
 //
 //  Created by 김지민 on 2021/10/11.
 //
-
+//
 #include <stdio.h>
+#include <string.h>
 void delchar(char [], char);
 
 int main() {
     char str[20] ="javajjjaklalakaka";
     char ch = 'a';
+    printf("원 문자열: %s\n",str);
     delchar(str,ch);
+    printf("delchar() 호출 이후 결과: %s\n",str);
+    return 0;
 }
 
 void delchar(char str[], char ch){
     char new[20];
-    int i=0;
-    while(*str){
-        if (*str!=ch)
-            new[i++]=*str;
-        str++;
+    int i,j=0;
+    for (i=0; i<strlen(str); j++){
+        if (str[j]!=ch)
+            new[i++]=str[j];
     }
-    printf("%s\n",new);
+    new[i]='\0';
+    strcpy(str,new);
 }
