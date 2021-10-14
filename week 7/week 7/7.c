acd684b9ba26cc7746c67d02e9e9563dea7480ab
Jimin Kim <0305jimin@gmail.com>
2021-10-15 04:00:45 +0900

    Refactor: week 7 #7
    removing redundant codes and specifying what the outcome means

--- week 7/week 7/7.c
+++ week 7/week 7/7.c
@@ -4,16 +4,13 @@
 //
 //  Created by 김지민 on 2021/10/12.
 //
-
+//
 #include <stdio.h>
 #include <stdlib.h>
 
 int main(int argc, char *argv[] ){
-    int i = 0;
     printf("실행 명령행 인자(command line arguments) >>\n");
-    printf("argc = %d\n", argc);
-    for (i = 0; i < argc; i++)
-           printf("argv[%d] = %s\n", i, argv[i]);
-    printf("%d+ %d=%d",atoi(argv[1]), atoi(argv[2]),atoi(argv[1])+ atoi(argv[2]));
+    printf("두 수의 합: %d + %d = %d\n",atoi(argv[1]), atoi(argv[2]),atoi(argv[1])+ atoi(argv[2]));
+    
     return 0;
 }
