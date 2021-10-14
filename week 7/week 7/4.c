4c53613c1bd729a0d1f21f3a20995d5a25979d19
Jimin Kim <0305jimin@gmail.com>
2021-10-14 23:15:33 +0900

    Refactor: week 7 #4
    1. using 'strlen' instead of counting the length of string by myself 2. changing the array s1, not using temporary array s3.

--- week 7/week 7/4.c
+++ week 7/week 7/4.c
@@ -10,28 +10,22 @@
 void mystrcat(char [], char []);
 
 int main() {
-    
     char s1[100] = "C ";
     char s2[50] = "programming language";
-    printf("(mystrcat) 연결된 결과: ");
+    char s3[100]="C ";//when using the function named 'mystrcat', s1 is changed. so I need a new array that contains "C ".
     mystrcat(s1,s2);
-    printf("  (strcat) 연결된 결과: %s\n",strcat(s1,s2));
+    printf("(mystrcat) 연결된 결과: %s\n",s1);
+    printf("  (strcat) 연결된 결과: %s\n",strcat(s3,s2));
     
     return 0;
 }
 
 void mystrcat(char s1[], char s2[]) {
-    int count=0;
-    char s3[100];
-    int i=0;
-    
-    while(*s1){
-        count++;
-        s3[i++]=*s1++;
-    }
+    int count = strlen(s1);//now I don't need to count the length of the string by myself, just using the function 'strlen'from string.h
+    //strlen: the length of the string. sizeof: lengthof the array
     while(*s2){
-        s3[count++]=*s2++;
+        s1[count++]=*s2++;
     }
-    s3[count]='\0';
-    printf("%s\n",s3);
+    
+    s1[count]='\0';
 }
