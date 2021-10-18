a09957db197f260048c2d5a26652871fc748b64c
Jimin Kim <0305jimin@gmail.com>
2021-10-19 00:21:23 +0900

    Feat: getting inputs and running according to the user's input

--- week 8/week 8/bank.c
+++ week 8/week 8/bank.c
@@ -12,11 +12,30 @@ void withdraw(int);
 int total=0;
 
 int main() {
+    int choice,money;
     printf("deposit   withdrawal   total-deposit   total-withdrawal   balance\n");
     printf("=================================================================\n");
     save(5000);
     save(24000);
     withdraw(3000);
+    printf("what are you going to do? 1: desposit 2: withdrawal >>");
+    scanf("%d",&choice);
+    while (choice!=1&&choice!=2){
+        printf("please choose a number between 1 or 2. \n");
+        printf("what are you going to do? 1: desposit 2: withdrawal >>");
+        scanf("%d",&choice);
+    }
+    printf("for how much? >>");
+    scanf("%d",&money);
+    if (choice==1){
+        printf("deposit   withdrawal   total-deposit   total-withdrawal   balance\n");
+        printf("=================================================================\n");
+        save(money);
+    }else{
+        printf("deposit   withdrawal   total-deposit   total-withdrawal   balance\n");
+        printf("=================================================================\n");
+        withdraw(money);
+    }
     return 0;
 }
 
