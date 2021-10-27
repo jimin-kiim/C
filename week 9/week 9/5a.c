ca4b8ce29f1253645d052c62b1368314f7e04635
Jimin Kim <0305jimin@gmail.com>
2021-10-27 21:42:40 +0900

    extern
    extern int x=10; occurs error. extern int x; x=10;  is right.

---
+++ week 9/week 9/5a.c
@@ -0,0 +1,18 @@
+//
+//  5a.c
+//  week 9
+//
+//  Created by 김지민 on 2021/10/27.
+//
+
+#include <stdio.h>
+//#include "5b.c"
+
+extern int x;
+
+int main(void){
+    
+    x=10;
+    printf("%d",x);
+    return 0;
+}
---
+++ week 9/week 9/5b.c
@@ -0,0 +1,10 @@
+//
+//  5b.c
+//  week 9
+//
+//  Created by 김지민 on 2021/10/27.
+//
+//
+//#include <stdio.h>
+
+int x=1;
