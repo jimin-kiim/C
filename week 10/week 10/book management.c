eb5ffb301f98fb0726acc17565d6d9db5a97e3b0
Jimin Kim <0305jimin@gmail.com>
2021-11-04 01:19:44 +0900

    feat: deleting a book

--- week 10/week 10/book management.c
+++ week 10/week 10/book management.c
@@ -17,6 +17,8 @@ typedef struct {
 Book books[10]={{"C programming", "Tim", 529},{"Java programming", "Karen", 345}};
 
 int findindex(Book []);
+void add(int);
+void delete(int);
 
 int main(void){
     int input;
@@ -30,26 +32,30 @@ int main(void){
     for(int i=0; i<3; i++){
         printf("%-18s%-10s%d\n",books[i].title,books[i].writer,books[i].pages);
     }
-    printf("What are you going to do?\n");
-    printf("1 : Add a new book, 2 : Delete a book, 3: Find a book >>");
-    scanf("%d",&input);
-    int i=findindex(books);
-    switch(input){
-        case(1):
-            printf("Please enter data of book\n");
-            printf("title: ");
-            scanf("%s",books[i].title);
-            printf("writer: ");
-            scanf("%s",books[i].writer);
-            printf("the number of pages: ");
-            scanf("%d",&books[i].pages);
+    
+    while(1){
+        printf("What are you going to do?\n");
+        printf("1 : Add a new book, 2 : Delete a book, 3: Find a book, 4: exit >>");
+        scanf("%d",&input);
+        int i=findindex(books);
+        switch(input){
+            case(1):
+                add(i);
+                break;
+            case(2):
+                printf("Which book do you want to delete?");
+                scanf("%d",&input);
+                delete(input);
+                break;
+            case(4):
+                return 0;
+        }
+        printf("title             writer    pages\n");
+        printf("=================================\n");
+        for(int j=0; j<=i; j++){
+            printf("%-18s%-10s%d\n",books[j].title,books[j].writer,books[j].pages);
+        }
     }
-    printf("title             writer    pages\n");
-    printf("=================================\n");
-    for(int j=0; j<=i; j++){
-        printf("%-18s%-10s%d\n",books[j].title,books[j].writer,books[j].pages);
-    }
-    return 0;
 }
 
 int findindex(Book a[]){
@@ -58,4 +64,20 @@ int findindex(Book a[]){
         i++;
     }
     return i;
+}
+
+void add(int i){
+    printf("Please enter data of book\n");
+    printf("title: ");
+    scanf("%s",books[i].title);
+    printf("writer: ");
+    scanf("%s",books[i].writer);
+    printf("the number of pages: ");
+    scanf("%d",&books[i].pages);
+}
+
+void delete(int i){
+    strcpy(books[i].title,"\0");
+    strcpy(books[i].writer,"\0");
+    books[i].pages=0;
 }
