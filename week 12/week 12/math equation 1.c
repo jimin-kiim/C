b43e554099f34bd54086196c5dfea7936c978a38
Jimin Kim <0305jimin@gmail.com>
2021-11-19 15:24:13 +0900

    Feat: two math equations
    using function pointer

--- week 12/week 12/math equation 1.c
+++ week 12/week 12/math equation 1.c
@@ -8,24 +8,30 @@
 #include <stdio.h>
 #include <math.h>
 
-double sigma(int);
-double f(int);
+double sigma(double (*pf)(int),int);
+double f1(int);
+double f2(int);
 
 int main(void){
     int n;
     printf("Enter a integer >>");
     scanf("%d",&n);
-    printf("summation from 1 to n (f^2+f+1): %lf\n",sigma(n));
+    printf("summation from 1 to n (f^2+f+1) where f=cos(x): %lf\n",sigma(f1,n));
+    printf("summation from 1 to n (f^2+f+1) where f=1/x: %lf\n",sigma(f2,n));
 }
 
-double sigma(int n){
+double sigma(double (*pf)(int), int n){
     double sum=0;
     for (int k=1; k<=n; k++){
-        sum+=pow(f(k),2)+f(k)+1;
+        sum+=pow(pf(k),2)+pf(k)+1;
     }
     return sum;
 }
 
-double f(int k){
+double f1(int k){
     return cos(k);
+}
+
+double f2(int k){
+    return 1/k;
 }
