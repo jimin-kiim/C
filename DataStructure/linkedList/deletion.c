283562b11a37a6fb37a246f4475065c3e53a7b7e
Jimin Kim <0305jimin@gmail.com>
2022-06-18 20:06:35 +0900

    [Linked List] deletion function

--- DataStructure/linkedList/deletion.c
+++ DataStructure/linkedList/deletion.c
@@ -7,40 +7,52 @@ typedef struct node {
     char *word;
     nodePointer link;
 };
+void delete(nodePointer *first, nodePointer trail, nodePointer x);
 
-nodePointer A=NULL;
 
 int main(int argc, const char * argv[]){
-    A=(nodePointer)malloc(sizeof(*A)); 
+    nodePointer A=NULL;
+    A=(nodePointer)malloc(sizeof(*A));
     A->word = "the";
 
-    nodePointer B = (nodePointer)malloc(sizeof(struct node)); 
+    nodePointer B = (nodePointer)malloc(sizeof(struct node));
     B->word = "lord";
     A->link = B;
 
-    nodePointer C = (nodePointer)malloc(sizeof(struct node)); 
+    nodePointer C = (nodePointer)malloc(sizeof(struct node));
     C->word = "of";
     B->link = C;
 
-    nodePointer D = (nodePointer)malloc(sizeof(struct node)); 
+    nodePointer D = (nodePointer)malloc(sizeof(struct node));
     D->word="rings";
-    C->link= NULL;
-
-    //delete B 
-    nodePointer deletion=B; //remember to free soon. 
-    A->link=B->link; // D should not be null. unless, we cannot access link field. 
+    C->link= D;
+    D->link=NULL;
+#if 0
+    //delete B
+    nodePointer deletion=B; //remember to free soon.
+    A->link=B->link; // D should not be null. unless, we cannot access link field.
     free(deletion);
     
-
     //delete A
-    nodePointer deletion=A;
-    A=A->link;  // A should not be null. unless, we cannot access link field. 
-    free(deletion);
-    
-    // nodePointer t = A;
-    // while(t!=NULL){
-    //     printf("word : %s\n",t->word);
-    //     t=t->link;
-    // }
-    printf("couldn't find");
+    nodePointer deletion2=A;
+    A=A->link;  // A should not be null. unless, we cannot access link field.
+    free(deletion2);
+#endif
+    delete(&A,NULL,A);
+    delete(&A,A,A->link);
+    nodePointer t = A;
+    while(t!=NULL){
+        printf("word : %s\n",t->word);
+        t=t->link;
+    }
+}
+void delete(nodePointer *first, nodePointer trail, nodePointer x){
+    // trail : the node infront of the target
+    // x : deletion target
+    if(trail){
+        trail->link=x->link;
+    }else{ // trail is NULL. deleting first node.
+        *first=(*first)->link;
+    }
+    free(x);
 }
